12d826757e359261a0ff5a1343f4c1fb68b72ed6
alexandrugavrila <42613563+alexandrugavrila@users.noreply.github.com>
2025-08-10 17:50:19 -0400

    Make DB configuration configurable

--- Backend/backend.py
+++ Backend/backend.py
@@ -1,3 +1,5 @@
+import os
+
 from flask import Flask
 from flask_cors import CORS
 
@@ -13,11 +15,15 @@ app.register_blueprint(ingredient_blueprint)
 app.register_blueprint(meal_blueprint)
 
 CORS(app)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://nutrition_user:nutrition_pass@nutrition-db:5432/nutrition'
+app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv(
+    'SQLALCHEMY_DATABASE_URI',
+    'postgresql://nutrition_user:nutrition_pass@nutrition-db:5432/nutrition'
+)
 db.init_app(app)
 
-with app.app_context():
-    db.create_all()
+if os.getenv('DB_AUTO_CREATE', '').lower() in ('1', 'true', 't'):
+    with app.app_context():
+        db.create_all()
 
 if __name__ == '__main__':
     app.run(debug = True, host='0.0.0.0')
